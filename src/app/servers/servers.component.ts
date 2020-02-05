49b62605d79c854aceb5db6306fb13f75a6b75e1
gubbalra <ramesh.gubbala9@gmail.com>
2020-02-05 13:53:20 +0400

    1 file modified, 2 added

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  serverName:String ='Server Name';
+  serverStatus:String='';
+  allowNewServer:Boolean=false;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
